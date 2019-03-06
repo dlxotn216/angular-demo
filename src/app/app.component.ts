1352d9c99e18c9b64be24bcb9e2eb3443fdc32fb
Lee TaeSu <taesu@crscube.co.kr>
2019-03-06 23:08:34 +0900

    Initial commit
    Angular material 맛보기
    Autocomplete 적용하기
    
    Observable을 이용한 비동기 파이프 사용 및 API 연동 예제

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs/index";
+import {map, startWith} from "rxjs/internal/operators";
+
+interface Option {
+  label: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-demo';
+  myControl: FormControl = new FormControl();
+  options: Option[] = [];
+  filteredOptions: Observable<Option[]>;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.initPipe();
+  }
+
+  private _reloadUserOptions() {
+    console.log('check call api');
+    setTimeout(() => {
+      this.options = [
+        {label: 'taesu', value: 'taesu@crscube.co.kr'},
+        {label: 'soyoung', value: 'soyoung@crscube.co.kr'},
+        {label: 'test', value: 'test@crscube.co.kr'},
+        {label: 'gavy', value: 'gavy@crscube.co.kr'}
+      ];
+      this.initPipe();
+    }, 1000);
+  }
+
+  private initPipe() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(this.myControl.value == null ? '' : this.myControl.value),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): Option[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.value.toLowerCase().includes(filterValue));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatAutocompleteModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -10,6 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
     AppRoutingModule
   ],
   providers: [],
